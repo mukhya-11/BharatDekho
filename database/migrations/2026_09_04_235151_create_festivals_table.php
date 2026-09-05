9676dddc9d57699cbb15b7090cad463359ecf92b
Mukhyajit Gogoi <mukhyajit11@gmail.com>
2026-09-05 05:30:49 +0530

    add neccessary tables

---
+++ database/migrations/2026_09_04_235151_create_festivals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('festivals', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('state_id')
+                  ->constrained('states')
+                  ->cascadeOnDelete();
+
+            $table->foreignId('pic_id')
+                  ->nullable()
+                  ->constrained('pictures')
+                  ->nullOnDelete();
+
+            $table->string('name');
+            $table->longText('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('festivals');
+    }
+};
